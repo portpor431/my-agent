aa301bd934d8874dc320db26906daf2992711843
portpor431 <portpor431@atomicmail.io>
2026-08-31 14:33:32 +0330

    Create agent.py

---
+++ agent.py
@@ -0,0 +1,12 @@
+import platform
+import sys
+
+print("🤖 Agent started!")
+print()
+
+print("Python:", sys.version)
+print("OS:", platform.system())
+print("Machine:", platform.machine())
+
+print()
+print("Agent is running successfully.")
